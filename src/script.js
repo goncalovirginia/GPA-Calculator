783808c6ea08f807118e968f8de97a1a35045715
xXParagoNXx <gonvirginia.blueslime@gmail.com>
2023-02-09 07:27:24 +0000

    refactored and redesigned everything

--- src/script.js
+++ src/script.js
@@ -1,9 +1,6 @@
 const gpaDisplay = document.getElementById("gpaDisplay");
-const nameInput = document.getElementById("name");
-const creditsInput = document.getElementById("credits");
-const gradeInput = document.getElementById("grade");
 
-document.getElementById("add").addEventListener("click", addSubjectThroughInput);
+document.getElementById("add").addEventListener("click", addEmptySubjectRow);
 document.getElementById("clear").addEventListener("click", clearSubjects);
 document.getElementById("import").addEventListener("click", readFileAsString);
 document.getElementById("export").addEventListener("click", exportSubjects);
@@ -23,64 +20,108 @@ class Subject {
 
 }
 
-function updateGPADisplay() {
-	gpaDisplay.innerHTML = (totalWeight / totalCredits).toFixed(2);
-	if (totalCredits == 0) gpaDisplay.innerHTML = 0;
+addSubjectRow("DSA", 9, 19);
+addSubjectRow("OSF", 9, 15);
+addSubjectRow("CN", 6, 16);
+addEmptySubjectRow();
+
+function addEmptySubjectRow() {
+	addSubjectRow("", "", "");
 }
 
-function addSubjectThroughInput() {
-	addSubject(nameInput.value, Number(creditsInput.value), Number(gradeInput.value));
-}
-
-function addSubject(name, credits, grade) {
-	let subject = new Subject(name, credits, grade);
-
-	if (subject.name.length < 1 || subject.credits < 1 || subject.grade < 1) {
-		alert("Subject name must be at least 1 character long.\nCredits and Grade must be at least 1.\nExample: ABC 6 18");
-		return;
-	}
-
-	nameInput.value = "";
-	creditsInput.value = "";
-	gradeInput.value = "";
-
+function addSubjectRow(name, credits, grade) {
 	let tr = document.createElement("tr");
 	tr.setAttribute("class", "subjects-tr");
 
-	for (item of [subject.name, subject.credits, subject.grade]) {
-		let td = document.createElement("td");
-		td.setAttribute("class", "subjects-td");
-		td.appendChild(document.createTextNode(item));
-		tr.appendChild(td);
+	let nameInput = document.createElement("input");
+	nameInput.setAttribute("type", "text");
+	nameInput.setAttribute("id", "name");
+	nameInput.setAttribute("placeholder", "Subject");
+	nameInput.value = name;
+
+	let td = document.createElement("td");
+	td.appendChild(nameInput);
+	tr.appendChild(td);
+
+	let creditsInput = document.createElement("input");
+	creditsInput.setAttribute("type", "number");
+	creditsInput.setAttribute("class", "credits");
+	creditsInput.setAttribute("id", "credits");
+	creditsInput.setAttribute("placeholder", "Credits");
+	creditsInput.setAttribute("min", "1");
+	creditsInput.addEventListener("input", updateGPA);
+	creditsInput.value = credits;
+
+	td = document.createElement("td");
+	td.appendChild(creditsInput);
+	tr.appendChild(td);
+
+	let gradeInput = document.createElement("select");
+	gradeInput.setAttribute("type", "number");
+	gradeInput.setAttribute("class", "grade");
+	gradeInput.setAttribute("id", "grade");
+	gradeInput.setAttribute("placeholder", "Grade");
+	gradeInput.addEventListener("input", updateGPA);
+
+	let option = document.createElement("option");
+	option.setAttribute("value", "");
+	option.setAttribute("hidden", "");
+	option.setAttribute("selected", "");
+	option.innerHTML = "Grade";
+	gradeInput.appendChild(option);
+
+	for (let i = 10; i < 21; i++) {
+		option = document.createElement("option");
+		option.setAttribute("value", i);
+		option.innerHTML = i;
+		gradeInput.appendChild(option);
 	}
+
+	if (grade != "") {
+		gradeInput.value = grade;
+	}
+
+	td = document.createElement("td");
+	td.appendChild(gradeInput);
+	tr.appendChild(td);
 
 	let removeButton = document.createElement("button");
 	removeButton.setAttribute("id", "remove");
 	removeButton.addEventListener("click", removeSubject);
 	removeButton.innerHTML = '<i class="gg-trash"></i>';
 	tr.appendChild(removeButton);
+
 	table.appendChild(tr);
 
-	totalWeight += subject.weight;
-	totalCredits += subject.credits;
-	updateGPADisplay();
+	updateGPA();
 }
 
 function removeSubject() {
-	let subject = table.removeChild(event.target.parentElement).children;
-	totalWeight -= subject[1].innerHTML * subject[2].innerHTML;
-	totalCredits -= Number(subject[1].innerHTML);
-	updateGPADisplay();
+	table.removeChild(event.target.parentElement);
+	updateGPA();
+}
+
+function updateGPA() {
+	totalWeight = totalCredits = 0;
+	let creditsInputs = table.getElementsByClassName("credits");
+	let gradeInputs = table.getElementsByClassName("grade");
+
+	for (let i = 0; i < creditsInputs.length; i++) {
+		if (creditsInputs[i].value == "" || gradeInputs[i].value == "") {
+			continue;
+		}
+
+		totalWeight += creditsInputs[i].value * gradeInputs[i].value;
+		totalCredits += Number(creditsInputs[i].value);
+	}
+
+	gpaDisplay.innerHTML = (totalWeight / totalCredits).toFixed(2);
+	if (totalCredits == 0) gpaDisplay.innerHTML = 0;
 }
 
 function clearSubjects() {
-	while (table.children.length > 1) {
-		table.removeChild(table.lastChild);
-	}
-
-	totalWeight = 0;
-	totalCredits = 0;
-	gpaDisplay.innerHTML = 0;
+	table.innerHTML = "";
+	updateGPA();
 }
 
 function readFileAsString() {
@@ -106,12 +147,12 @@ function importSubjects(content) {
 
 	for (line of content.split("\n")) {
 		let items = line.split(" ");
-		addSubject(items[0], Number(items[1]), Number(items[2]));
+		addSubjectRow(items[0], Number(items[1]), Number(items[2]));
 	}
 }
 
 function exportSubjects() {
-	if (table.children.length < 2) {
+	if (table.children.length < 1) {
 		alert("There must be at least 1 subject in the list.");
 		return;
 	}
@@ -119,12 +160,18 @@ function exportSubjects() {
 	let content = "";
 
 	for (row of table.getElementsByTagName("tr")) {
-		content += row.children[0].innerHTML + " " + row.children[1].innerHTML + " " + row.children[2].innerHTML + "\n";
+		let name = row.children[0].children[0].value;
+		let credits = row.children[1].children[0].value;
+		let grade = row.children[2].children[0].value;
+
+		console.log(name);
+
+		if (name != "" && credits != "" && grade != "") {
+			content += name + " " + credits + " " + grade + "\n";
+		}
 	}
 
-	content = content.substring(content.indexOf("\n")+1);
-
-	var element = document.createElement('a');
+	let element = document.createElement('a');
   	element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(content));
   	element.setAttribute('download', 'subjects');
 
